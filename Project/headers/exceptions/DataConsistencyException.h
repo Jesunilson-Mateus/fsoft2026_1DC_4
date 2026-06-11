025572cd57d378b3efbce8ae00470a3b8bdd5784
Jesunilson-Mateus <1241774@isep.ipp.pt>
2026-06-11 11:28:02 +0100

    Implementar DataConsistencyException customizada

--- Project/headers/exceptions/DataConsistencyException.h
+++ Project/headers/exceptions/DataConsistencyException.h
@@ -1,8 +1,19 @@
-//
-// Created by LarsenTivane on 17/05/2026.
-//
-
 #ifndef FSOFT2026_1DC_4_DATACONSISTENCYEXCEPTION_H
 #define FSOFT2026_1DC_4_DATACONSISTENCYEXCEPTION_H
+
+#include <exception>
+#include <string>
+
+class DataConsistencyException : public std::exception {
+private:
+    std::string mensagem;
+
+public:
+    explicit DataConsistencyException(const std::string& msg) : mensagem(msg) {}
+
+    const char* what() const noexcept override {
+        return mensagem.c_str();
+    }
+};
 
 #endif //FSOFT2026_1DC_4_DATACONSISTENCYEXCEPTION_H
