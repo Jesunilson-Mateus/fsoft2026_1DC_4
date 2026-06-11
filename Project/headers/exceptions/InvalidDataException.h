38b1b45b4849839ad579afdff6af5088a8902992
Jesunilson-Mateus <1241774@isep.ipp.pt>
2026-06-11 11:24:39 +0100

    Implementar InvalidDataException customizada

--- Project/headers/exceptions/InvalidDataException.h
+++ Project/headers/exceptions/InvalidDataException.h
@@ -1,8 +1,19 @@
-//
-// Created by LarsenTivane on 17/05/2026.
-//
-
 #ifndef FSOFT2026_1DC_4_INVALIDDATAEXCEPTION_H
 #define FSOFT2026_1DC_4_INVALIDDATAEXCEPTION_H
+
+#include <exception>
+#include <string>
+
+class InvalidDataException : public std::exception {
+private:
+    std::string mensagem;
+
+public:
+    explicit InvalidDataException(const std::string& msg) : mensagem(msg) {}
+
+    const char* what() const noexcept override {
+        return mensagem.c_str();
+    }
+};
 
 #endif //FSOFT2026_1DC_4_INVALIDDATAEXCEPTION_H
